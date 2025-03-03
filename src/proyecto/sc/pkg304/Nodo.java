19dc7251d0e53f9223125e3d2e84a01f0ac1e9e2
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2025-03-02 21:19:40 -0600

    Implement Cola and Nodo class

---
+++ src/proyecto/sc/pkg304/Nodo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.sc.pkg304;
+
+/**
+ *
+ * @author jafeth.garro
+ */
+public class Nodo {
+    private Tiquete tiqete;
+    private Nodo sig;
+
+    public Nodo(Tiquete tiquete) {
+        this.tiqete = tiquete;
+    }
+
+    public Tiquete getNombre() {
+        return tiqete;
+    }
+
+    public void setNombre(Tiquete tiquete) {
+        this.tiqete = tiquete;
+    }
+
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" + "tiquete=" + tiqete + "}";
+    }
+    
+    
+}
