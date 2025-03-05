b579f065914f3d205f0eded7b1e4d8033938ca27
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2025-03-04 21:39:16 -0600

    Add test cases in the main method

--- src/proyecto/sc/pkg304/ProyectoSC304.java
+++ src/proyecto/sc/pkg304/ProyectoSC304.java
@@ -14,7 +14,26 @@ public class ProyectoSC304 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cola cola = AdministradorArchivo.cargarCola();
+        cola.imprimirDetalles();
+        System.out.println("Cola cargada: " + cola);
+        
+        cola.encola(new Tiquete("Juan Perez", 1, 30, TipoTramite.DEPOSITOS, TipoTiquete.A));
+        cola.encola(new Tiquete("Maria Lopez", 2, 45, TipoTramite.RETIROS, TipoTiquete.B));
+        cola.encola(new Tiquete("Carlos Jimenez", 3, 50, TipoTramite.CAMBIO_DIVISAS, TipoTiquete.P));
+        
+        System.out.println("Cola después de agregar tiquetes: " + cola);
+        cola.imprimirDetalles();
+        try {
+            System.out.println("Atendiendo: " + cola.atiende().getNombre());
+            System.out.println("Atendiendo: " + cola.atiende().getNombre());
+        } catch (Exception e) {
+            System.err.println("Error al atender un tiquete: " + e.getMessage());
+        }
+        
+        System.out.println("Cola después de atender tiquetes: " + cola);
+        cola.imprimirDetalles();
+        AdministradorArchivo.guardarCola(cola);
     }
     
 }
